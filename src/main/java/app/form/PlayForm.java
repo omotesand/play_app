32b9362fd80a1e9d7eef9c2b0928740a1b1234ed
omotesand <kureso17chokkenneyo@gmail.com>
2022-03-26 11:34:32 +0900

    20220326 Add validation in PlayForm.java

--- src/main/java/app/form/PlayForm.java
+++ src/main/java/app/form/PlayForm.java
@@ -1,8 +1,13 @@
 package app.form;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class PlayForm {
+	@NotNull (message = "投稿する文章を入力してください。")
+	@Size(min = 10, max = 120, message = "10文字以上120文字以下で入力してください。")
 	private String text;
 }
