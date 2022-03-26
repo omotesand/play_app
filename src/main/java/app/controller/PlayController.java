1566a2a63a5e863a5905cfa0c01ea09e3dccf179
omotesand <kureso17chokkenneyo@gmail.com>
2022-03-26 10:21:22 +0900

    20220326 Add the resultPage method in PlayController.java

--- src/main/java/app/controller/PlayController.java
+++ src/main/java/app/controller/PlayController.java
@@ -1,12 +1,17 @@
 package app.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.entity.Play;
+import app.form.PlayForm;
 import app.service.DetectSentiment;
 import app.service.PlayService;
 
@@ -40,6 +45,7 @@ public class PlayController {
 	@GetMapping("/form")
 	public String formPage(Model model) {
 		Play play = playService.getText();
+		model.addAttribute("title", "文章を入力してください");
 		model.addAttribute("play", play);
 		return "form";
 	}
@@ -48,10 +54,20 @@ public class PlayController {
 	 * 結果表示画面
 	 */
 	@GetMapping("/result")
-	public String resultPage(Model model) {
-		float score = detectSentiment.amazonComprehend();
-		model.addAttribute("score", score);
-		return "result";
+	public String resultPage(
+			@Valid @ModelAttribute PlayForm playForm,
+			BindingResult result,
+			Model model) {
+		if (!result.hasErrors()) {
+			play.setText(playForm.getText());
+			float score = detectSentiment.amazonComprehend(playForm.getText());
+			model.addAttribute("score", score);
+			return "redirect:/play/result";
+		} else {
+			model.addAttribute("title", "文章を入力し直してください");
+			return "form";
+		}
+
 	}
 
 }
