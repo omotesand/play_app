b4643f15517471115a59cdbf3f8a352c504bb146
omotesand <kureso17chokkenneyo@gmail.com>
2022-03-27 21:41:10 +0900

    20220327 Change the table name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 --事前登録のお題データはDBeaverから直接入力
---INSERT INTO playtable(text)
---               VALUES("モコ"),
---                     ("ポチ"),
---                     ("シロ"),
---                     ("チョコ");
+INSERT INTO play_table(challenge)
+               VALUES("プログラミングを勉強してみたら、"),
+                     ("初めてやったのがC言語で、"),
+                     ("今日の夜ご飯は"),
+                     ("最近Twitterを始め"),
+                     ("今年の夏は例年より暑くなると");
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 -- #timetableがあれば削除
-DROP TABLE IF EXISTS playTable;
+DROP TABLE IF EXISTS play_table;
 
 -- timetableがなければ新しく作成
-CREATE TABLE IF NOT EXISTS playTable(
+CREATE TABLE IF NOT EXISTS play_table(
 id INT AUTO_INCREMENT,
 challenge VARCHAR(50) NOT NULL,
 created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
