9c4649927428cc83c8a96e8c54f8b8f7350ec911
omotesand <kureso17chokkenneyo@gmail.com>
2022-04-03 10:11:25 +0900

    20220403 Create inputs table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,8 +4,19 @@ DROP TABLE IF EXISTS challenges;
 -- tableがなければ新しく作成
 CREATE TABLE IF NOT EXISTS challenges(
 challenge_id INT AUTO_INCREMENT,
--- sentiment_type
-challenge VARCHAR(50) NOT NULL,
--- created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+challenge VARCHAR(120) NOT NULL,
 PRIMARY KEY(challenge_id)
+);
+
+-- -------------------------------------------------------
+DROP TABLE IF EXISTS inputs;
+
+-- tableがなければ新しく作成
+CREATE TABLE IF NOT EXISTS inputs(
+input_id INT AUTO_INCREMENT,
+-- sentiment_type
+-- input
+score DECIMAL(4, 3) NOT NULL,
+-- created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(input_id)
 );
