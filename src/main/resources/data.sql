9a37383dc3693cd1a3c163ced4c4c8400ac06cdd
omotesand <kureso17chokkenneyo@gmail.com>
2022-04-03 10:30:40 +0900

    20220403 Add data to inputs table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,13 @@
 -- challngesテーブル
 INSERT INTO challenges(challenge)
-               VALUES("プログラミングを勉強してみたら、"),
-                     ("初めてやったのがC言語で、"),
-                     ("今日の夜ご飯は"),
-                     ("最近Twitterを始め"),
-                     ("今年の夏は例年より暑くなると");
+            VALUES("プログラミングを勉強してみたら、"),
+                  ("初めてやったのがC言語で、"),
+                  ("今日の夜ご飯は"),
+                  ("最近Twitterを始め"),
+                  ("今年の夏は例年より暑くなると");
+
+-- inputsテーブル
+INSERT INTO inputs(score)
+            VALUES(0.971),
+                  (0.876),
+                  (0.573);
